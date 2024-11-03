d2e008cae5bc5c03dc53e9c86ddd7e0232129e5e
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-11-03 17:19:31 +0530

    after long time change

---
+++ 11TodoContextLocal/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AvatarWithText } from './components/AvatarWithText'
+import { ExampleNavbarThree } from './components/ExampleNavbarThree'
+
+const App = () => {
+  return (
+    <>
+     <ExampleNavbarThree />
+      <AvatarWithText />
+    </>
+  )
+}
+
+export default App
