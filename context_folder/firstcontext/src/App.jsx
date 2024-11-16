cb7bbd80a5f0b6a2aefcee532a4aaa6bce332762
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-11-16 16:33:43 +0530

    review the work

---
+++ context_folder/firstcontext/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const App = () => {
+
+  const name_array =[
+    {
+      name:"shriyash",
+      age:21,
+      city:"New York"
+    },
+    {
+      name:"Batman",
+      age:22,
+      city:"New York"
+    },
+   
+  ]
+
+  return (
+    <div>
+      <h3>hey I am shriyash</h3>
+    </div>
+  )
+}
+
+export default App
